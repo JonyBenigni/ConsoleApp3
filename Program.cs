393edcaac366bd1ec6a01f5ac7ba5127bff57fa9
Usuario <Usuario@JONATHANBENIGNI>
2022-07-04 18:33:07 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class simulacionjonathan
+{
+
+	static void Main(string[] args)
+	{
+		string am;
+		int horas;
+		int min;
+		string pm;
+		int seg;
+		Console.Write(" Ingrese la cantidad de horas ");
+		horas = int.Parse(Console.ReadLine());
+		Console.Write(" Ingrese la cantidad de minutos ");
+		min = int.Parse(Console.ReadLine());
+		Console.Write(" Ingrese la cantidad de segundos ");
+		seg = int.Parse(Console.ReadLine());
+		Console.Write("Formato 24hs ");
+		if (horas < 12)
+		{
+			Console.WriteLine(horas + ":" + min + ":" + seg + "am");
+		}
+		else
+		{
+			Console.WriteLine(horas + ":" + min + ":" + seg + "pm");
+		}
+	}
+
+}
+
+
+
+
